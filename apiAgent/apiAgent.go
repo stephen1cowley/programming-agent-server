33c66092314a3b8c7efd9fc6631e74a37f683678
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-09-15 11:13:49 +0100

    http server first attempt

--- apiAgent/apiAgent.go
+++ apiAgent/apiAgent.go
@@ -1,9 +1,210 @@
 package apiAgent
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"regexp"
+	"time"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/sashabaranov/go-openai"
+	funcTools "github.com/stephen1cowley/programming-agent-server/funcTools"
 )
 
+type msgSchema struct {
+	Message string `json:"message"`
+}
+
+// Global variables (for now...)
+var apiKey string
+var client openai.Client
+var messages []openai.ChatCompletionMessage
+var currDirState funcTools.DirectoryState
+var startSysMsg openai.ChatCompletionMessage
+var myTools []openai.Tool
+var editAppJSResp funcTools.ArgsAppJS
+var editAppCSSResp funcTools.ArgsAppCSS
+var newFileResp funcTools.ArgsCreateFile
+var libsResp funcTools.ArgsLibraries
+
 func ApiAgent() {
-	fmt.Println("Hello")
+	// Start off by cleaning the React App source code
+	cmd := exec.Command("shell_script/onStartup.sh")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println(string(output))
+
+	// Initialise chat variables
+	apiKey = os.Getenv("OPEN_AI_API_KEY")
+	client = *openai.NewClient(apiKey)
+	messages = make([]openai.ChatCompletionMessage, 0)
+	currDirState = funcTools.DirectoryState{} // i.e. initially empty
+	startSysMsg = openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleSystem,
+		Content: "You are a helpful software engineer. Currently we are working on a fresh React App boilerplate, with access to Bootstrap 5 styles. You are able to change App.js and App.css. You are able to create new JavaScript files to assist you in creating the application, ensure these are correctly imported into App.js.",
+	} // Starting system message always prepended to list of messages
+	myTools = []openai.Tool{funcTools.AppJSEdit, funcTools.AppCSSEdit, funcTools.NewJsonFile}
+
+	// Now we can begin the conversation by opening up the server!
+	http.HandleFunc("/", testHandler)
+	http.HandleFunc("/api/message/", apiMessageHandler)
+
+	fmt.Println("Server listening on :8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
+}
+
+func apiMessageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(currDirState)
+	if r.Method == http.MethodPost {
+		fmt.Fprintln(w, "Post request received") // server debug message
+
+		var requestData msgSchema
+		if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintln(w, "Error decoding request data:", err)
+			return
+		}
+
+		// Process data after successful unmarshalling
+		responseMessage := "Received POST request with data: " + requestData.Message
+		text := requestData.Message
+		messages = append(messages, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleUser,
+			Content: text,
+		})
+
+		// System message at the end describing the current state of the files
+		endSysMsg := openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleSystem,
+			Content: currDirState.CreateSysMsgState(),
+		}
+
+		// Start and end system message with user/machine communication sandwiched inbetween
+		messagesWithSys := append(append([]openai.ChatCompletionMessage{startSysMsg}, messages...), endSysMsg)
+		fmt.Println(messagesWithSys)
+
+		// Define a regular expression pattern to match everything between backticks
+		re := regexp.MustCompile("```[^```]+```")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		resp, err := client.CreateChatCompletion(
+			ctx,
+			openai.ChatCompletionRequest{
+				Model:       openai.GPT4o,
+				Messages:    messagesWithSys,
+				Tools:       myTools,
+				Temperature: 0.8,
+				// ToolChoice: "required",
+			},
+		)
+		cancel()
+
+		if err != nil {
+			fmt.Printf("ChatCompletion error: %v\n", err)
+			return
+		}
+
+		content := resp.Choices[0].Message.Content
+
+		tool_calls := resp.Choices[0].Message.ToolCalls
+
+		fmt.Println(content)
+		fmt.Println(messages)
+
+		if len(tool_calls) != 0 {
+			fmt.Println("Now making any tool calls ...")
+		}
+
+		// Replace all occurrences of stuff between ```...```
+		content = re.ReplaceAllString(content, "")
+
+		for _, val := range tool_calls {
+		outerSwitch:
+			switch val.Function.Name {
+			case "app_js_edit_func":
+				fmt.Println("Updating App.js ...")
+				json.Unmarshal([]byte(val.Function.Arguments), &editAppJSResp)
+				funcTools.EditAppJS(
+					editAppJSResp.AppJSCode,
+				)
+				currDirState.AppJSCode = editAppJSResp.AppJSCode
+			case "app_css_edit_func":
+				fmt.Println("Updating App.css ...")
+				json.Unmarshal([]byte(val.Function.Arguments), &editAppCSSResp)
+				funcTools.EditAppCSS(
+					editAppCSSResp.AppCSSCode,
+				)
+				currDirState.AppCSSCode = editAppCSSResp.AppCSSCode
+			case "new_js_file_func":
+				fmt.Println("Creating new JS file ...")
+				json.Unmarshal([]byte(val.Function.Arguments), &newFileResp)
+				funcTools.CreateJSFile(
+					newFileResp,
+				)
+				for i, file := range currDirState.OtherFiles {
+					if newFileResp.FileName == file.FileName {
+						currDirState.OtherFiles[i].FileCode = newFileResp.FileContent
+						break outerSwitch
+					}
+				}
+				// File doesn't yet exist in the list; append this new file.
+				currDirState.OtherFiles = append(
+					currDirState.OtherFiles,
+					funcTools.FileState{
+						FileName: newFileResp.FileName,
+						FileCode: newFileResp.FileContent,
+					})
+			case "libraries_func":
+				fmt.Println("Importing libraries ...")
+				json.Unmarshal([]byte(val.Function.Arguments), &libsResp)
+				funcTools.InstallLibraries(
+					libsResp,
+				)
+			}
+		}
+
+		if len(messages) >= 10 {
+			messages = messages[2:]
+		}
+
+		messages = append(messages, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleAssistant,
+			Content: content,
+		})
+
+		fmt.Println(messages)
+
+		// Create output and respond (same as input schema for now...)
+		jsonResponse := msgSchema{Message: responseMessage}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(jsonResponse); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintln(w, "Error encoding JSON response", err)
+			return
+		}
+
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "Method not allowed") // server debug message
+	}
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		fmt.Fprintln(w, "Post request received")
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "Method not allowed") // server debug message
+	}
 }
