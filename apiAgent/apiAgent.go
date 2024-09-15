d2bda3982552eaf4188241c36836ca4a3f03f3db
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-09-15 14:27:16 +0100

    unblock CORS policy

--- apiAgent/apiAgent.go
+++ apiAgent/apiAgent.go
@@ -216,7 +216,7 @@ func apiRestartHandler(w http.ResponseWriter, r *http.Request) {
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost") // Replace with your allowed origin(s)
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // Replace with your allowed origin(s)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
