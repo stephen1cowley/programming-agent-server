589ac382ac6ee3ec3d9658f31f1e14e57c798af1
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-09-18 11:01:45 +0100

    Retry application stop

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Stop the server using the PID file
+# Stop the server using the PID file.
 if [ -f /home/ubuntu/server.pid ]; then
   kill $(cat /home/ubuntu/server.pid)
   rm /home/ubuntu/server.pid
