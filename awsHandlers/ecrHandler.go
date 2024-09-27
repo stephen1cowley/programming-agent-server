dc92ad30f86f59c2940b204396a6b6a22d086e1c
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-09-27 11:13:09 +0100

    More verbose error messages

--- awsHandlers/ecrHandler.go
+++ awsHandlers/ecrHandler.go
@@ -35,7 +35,7 @@ func getECRLogin(cfg aws.Config) error {
 		return fmt.Errorf("docker login failed: %v\noutput: %s", err, string(output))
 	}
 
-	fmt.Printf("Docker login %s %s successful for registry: %s\n", credentials[0], credentials[1], registry)
+	fmt.Printf("Docker login USERNAME: %s \n\n PASSWORD: %s\n\n successful for registry: %s\n", credentials[0], credentials[1], registry)
 	return nil
 }
 
