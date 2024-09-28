3622fd4bab51d1138f068157ed2d99e057f3ed77
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-09-28 18:07:20 +0100

    upload file to s3 fnc

--- awsHandlers/s3Handler.go
+++ awsHandlers/s3Handler.go
@@ -16,8 +16,9 @@ import (
 var s3Client *s3.Client
 
 const (
-	S3_REGION = "eu-west-2"
-	S3_BUCKET = "my-programming-agent-img-store"
+	S3_REGION     = "eu-west-2"
+	S3_BUCKET     = "my-programming-agent-img-store"
+	S3_BUCKET_APP = "test-stack-bucket-program-agent"
 )
 
 // InitS3 Creates a fresh S3 client, required for the running of the other S3 functions
@@ -54,6 +55,24 @@ func UploadToS3(file multipart.File, handler *multipart.FileHeader, userID strin
 	// Construct the file URL
 	fileURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", S3_BUCKET, S3_REGION, s3Path)
 	return fileURL, nil
+}
+
+func uploadFileToS3(filename string, content string) error {
+	// Convert the string content to bytes
+	buf := bytes.NewBufferString(content)
+
+	// Upload the file to S3
+	_, err := s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket:      aws.String(S3_BUCKET_APP),
+		Key:         aws.String(filename),
+		Body:        buf,
+		ContentType: aws.String("text/plain"), // Change as needed
+	})
+	if err != nil {
+		return fmt.Errorf("failed to upload file: %w", err)
+	}
+
+	return nil
 }
 
 // DeleteFromS3 deletes a file from S3 given its path and returns an error if any occurs
